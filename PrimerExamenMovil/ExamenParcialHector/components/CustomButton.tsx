ee13f9b4333e24bf70eab7a7f2250f697e8d50c1
DESKTOP-CKUTKR2\Hector Santos <hectorsantoslopez6@gmail.com>
2025-05-23 23:41:31 -0600

    update

---
+++ PrimerExamenMovil/ExamenParcialHector/components/CustomButton.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { TouchableOpacity, Text, StyleSheet } from "react-native";
+
+type Props = {
+  title: string;
+  onPress: () => void;
+  variant?: 'primary' | 'secondary';
+
+};
+
+
+export default function CustomButton({title, onPress, variant = 'primary'}: Props) {
+  const styles = getStyles('primary');
+  return(
+    <TouchableOpacity
+      style={styles.button}
+      onPress={onPress}
+    >
+      <Text style={styles.text}>{title}</Text>
+    </TouchableOpacity>
+  );
+}
+
+// Define styles for the button
+function getStyles(type: string) {
+  return StyleSheet.create({
+    button: {
+      backgroundColor: type === 'primary' ? '#007BFF' : '#6C757D',
+      padding: 10,
+      borderRadius: 5,
+      alignItems: 'center',
+    },
+    text: {
+      color: '#FFFFFF',
+      fontSize: 16,
+    },
+  });
+}
