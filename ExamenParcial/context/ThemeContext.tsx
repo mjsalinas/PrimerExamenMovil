683f9d50b169d226bc5211d094ec239ad319855e
AbrahamRP97 <abrahamj.rivera97@gmail.com>
2025-05-23 22:41:53 -0600

    ThemeContext added para activar/desactivar modo Oscuro en todas las pantallas

---
+++ ExamenParcial/context/ThemeContext.tsx
@@ -0,0 +1,31 @@
+import React, { createContext, useState, useContext, ReactNode } from "react";
+
+interface ThemeContextType {
+  isDarkMode: boolean;
+  toggleDarkMode: () => void;
+}
+
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
+
+
+export const ThemeProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode((prevMode) => !prevMode);
+  };
+
+  return (
+    <ThemeContext.Provider value={{ isDarkMode, toggleDarkMode }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = (): ThemeContextType => {
+  const context = useContext(ThemeContext);
+  if (!context) {
+    throw new Error("useTheme debe ser usado dentro de un ThemeProvider");
+  }
+  return context;
+};
