bd3b3e6d5a411cd30782b1d0432a3f4b7b264ba9
Cristina Lopez <Cristina.lopezm9@gmail.com>
2025-05-26 20:48:35 -0600

    Importando librerias de react navigation y pantallas profile y homeScreen.

--- ExamenParcial/App.tsx
+++ ExamenParcial/App.tsx
@@ -1,20 +1,23 @@
+
 import React from "react";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
-import ProfileScreen from "../ProfileScreen";
-import HomeScreen from "../screens/HomeScreen";
-// ⚠️ Error: SettingsScreen no importado
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import HomeScreen from "./screens/HomeScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+import SettingsScreen from "./screens/SettingsScreen";
+
 const Stack = createNativeStackNavigator();
 
-export const RootNavigator = () => {
+export default function App() {
   return (
     <NavigationContainer>
-
-    <Stack.Navigator initialRouteName="Home">
-      <Stack.Screen name="Home" component={HomeScreen} />
-      <Stack.Screen name="Profile" component={ProfileScreen} />
-    </Stack.Navigator>
+      <Stack.Navigator initialRouteName="Inicio">
+        <Stack.Screen name="Inicio" component={HomeScreen} />
+        <Stack.Screen name="Perfil" component={ProfileScreen} />
+        <Stack.Screen name="Configuración" component={SettingsScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
-
   );
-};
+}
+
