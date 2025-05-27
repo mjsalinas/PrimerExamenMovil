104c99139f725c1b268dadd70ef8c614127c2f12
Cristina Lopez <Cristina.lopezm9@gmail.com>
2025-05-26 20:25:14 -0600

    Corrigiendo errores, agregando estados con UseStare e Importando librerias.

--- ExamenParcial/screens/ProfileScreen.tsx
+++ ExamenParcial/screens/ProfileScreen.tsx
@@ -2,9 +2,9 @@ import React, { useState } from "react";
 import { View, TextInput, Button, Text } from "react-native";
 
 const ProfileScreen = () => {
-  const [nombre, setNombre] = useState();
+  const [nombre, setNombre] = useState("");
   const [edad, setEdad] = useState("");
-  const [bio] = useState(""); 
+  const [biografia, setBiografia] = useState("");
   const [saved, setSaved] = useState(false);
 
   const guardarPerfil = () => {
@@ -15,17 +15,31 @@ const ProfileScreen = () => {
 
   return (
     <View style={{ padding: 20 }}>
-      <TextInput placeholder="Nombre" />
-      <TextInput placeholder="Edad" />
-      <TextInput placeholder="Biografía" />
+      <TextInput placeholder="Nombre" value={nombre} onChangeText={setNombre} style={{ marginBottom: 10 }} />
+      <TextInput placeholder="Edad" value={edad} onChangeText={setEdad} keyboardType="numeric" style={{ marginBottom: 10 }} />
+      <TextInput placeholder="Biografía" value={biografia} onChangeText={setBiografia} style={{ marginBottom: 10 }} />
 
-      <Button title="Guardar" onPress={() => {}} />
-      {saved ? <Text>Guardado!</Text> : ""}
-      {saved && <View> 
-        <Text>Tu nombre es: {nombre}, tienes {edad} años</Text>
-        </View>}
+      <Button title="Guardar" onPress={guardarPerfil} />
+
+      {saved && (
+        <View style={{ marginTop: 10 }}>
+          <Text>Guardado!</Text>
+          <Text>Tu nombre es: {nombre}, tienes {edad} años</Text>
+          <Text>Biografia: {biografia}</Text>
+        </View>
+      )}
     </View>
   );
 };
 
 export default ProfileScreen;
+
+
+
+
+
+
+
+
+
+
